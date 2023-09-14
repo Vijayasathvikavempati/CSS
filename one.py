d19124232f3232b1d4aae7578b912402e1cbb61b
Vijayasathvikavempati <sathvikavijju@gmail.com>
2023-09-14 17:57:12 +0530

    working

---
+++ one.py
@@ -0,0 +1,51 @@
+''''price = 399
+print(price)
+print(type(price))
+
+rating = 4.9
+salary = 45000.00
+print(type(rating))
+print(type(salary))'''
+
+
+'''size=[]
+size=()
+size=[12,12,12,112]
+size={}
+size={}
+
+print(type(size))
+print(type(size))
+print(type(size))
+print(type(size))
+print(type(size))'''
+
+size =[]
+color=()
+ids1={11,11,12,12}
+ids2={
+    'name':102,
+    'age':22
+}
+
+
+print(size)
+print(type(color))
+print(ids1)
+print(type(ids2))
+
+
+'''# create a set of integer type
+size = [112, 111, 116, 118, 115]
+#print('Student ID:', student_id)
+
+# create a set of string type
+color = ['a', 'e', 'i', 'o', 'u']
+#print('Vowel Letters:', vowel_letters)
+
+# create a set of mixed data types
+ids1 = ['Hello', 101, -2, 'Bye']
+#print('Set of mixed data types:', mixed_set)print(type(size))
+print(type(color))
+print(type(ids1))
+print(type(size))'''
